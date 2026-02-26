69db13cc563af16a362e02c3e8013755956e4c2e
catthatmeows <zairysbigtae@gmail.com>
2026-02-27 00:33:39 +0800

    changed the way checking keys work

--- src/key.rs
+++ src/key.rs
@@ -17,51 +17,28 @@ impl KeyInput {
         }
     }
 
-    pub fn is_key_pressed<F>(&mut self, key: KeyCode, mut handler: F) -> bool
-    where
-        F: FnMut(),
-    {
-        self.previous = HashSet::from([key]);
+    pub fn update<F>(&mut self) {
+        self.previous = self.current.clone();
 
         if event::poll(Duration::from_millis(0)).unwrap()
             && let event::Event::Key(event) = event::read().unwrap()
         {
             match event.kind {
-                event::KeyEventKind::Press => {
-                    self.current.insert(event.code);
-                    if event.code == key {
-                        handler();
-                        return true;
-                    }
-                }
+                event::KeyEventKind::Press | event::KeyEventKind::Repeat => { self.current.insert(event.code); }
                 event::KeyEventKind::Release => { self.current.remove(&event.code); }
-                _ => {}
             }
         }
-        false
     }
 
-    pub fn is_key_released<F>(&mut self, key: KeyCode, mut handler: F) -> bool
-    where
-        F: FnMut(),
-    {
-        self.previous = HashSet::from([key]);
+    pub fn is_down<F>(&mut self, key: &KeyCode) -> bool {
+        self.current.contains(key)
+    }
 
-        if event::poll(Duration::from_millis(0)).unwrap()
-            && let event::Event::Key(event) = event::read().unwrap()
-        {
-            match event.kind {
-                event::KeyEventKind::Release => {
-                    self.current.remove(&event.code);
-                    if event.code == key {
-                        handler();
-                        return true;
-                    }
-                }
-                event::KeyEventKind::Press => { self.current.insert(event.code); }
-                _ => {}
-            }
-        }
-        false
+    pub fn is_pressed(&mut self, key: &KeyCode) -> bool {
+        self.current.contains(key) && !self.previous.contains(key)
+    }
+
+    pub fn is_released(&mut self, key: &KeyCode) -> bool {
+        !self.current.contains(key) && self.previous.contains(key)
     }
 }
